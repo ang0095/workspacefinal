2af1b1e23382230273ef1a98fa8aee5b3aba9939
Iamneo <techadmin@iamneo.ai>
2023-11-25 17:09:16 +0000

    Backup Commit

--- Laundry/angularapp/src/app/schedule-form/schedule-form.component.ts
+++ Laundry/angularapp/src/app/schedule-form/schedule-form.component.ts
@@ -1,56 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { FormBuilder, FormGroup } from '@angular/forms';
-// import { ScheduleService } from '../services/schedule-service.service';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { Userschedule } from 'src/models/userschedule.model';
-
-// @Component({
-//   selector: 'app-schedule-form',
-//   templateUrl: './schedule-form.component.html',
-//   styleUrls: ['./schedule-form.component.css']
-// })
-// export class ScheduleFormComponent implements OnInit {
-//   // scheduleForm : FormBuilder
-//   // userschedule : Userschedule
-//   scheduleForm=this.fb.group({
-//     fullName:[''],
-//     mobileNumber:[''],
-//     email:[''],
-//     address:[''],
-//     area:[''],
-//     pincode:[''],
-//     pickupDay:[''],
-//     pickupTimeSlot:[''],
-//     packageId:['']
-//   })
-
-//   constructor(private ss : ScheduleService , private fb: FormBuilder ) { }
-
-  
-
-//   ngOnInit(): void {
-//   }
-  
-//   // onSubmit() : void {
-//   // this.userschedule = this.scheduleForm.value
-//   // this.ss.createSchedule(this.userschedule).subscribe(
-//   //   () => {
-//   //     alert("Schedule created Successfully");
-      
-
-//   // });
-
-//   // }
-//   onSubmit() : void {
-
-//   }
-
-// }
-
-
-
-
-
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ScheduleService } from '../services/schedule-service.service';
@@ -63,26 +10,23 @@ import { ScheduleService } from '../services/schedule-service.service';
 export class ScheduleFormComponent implements OnInit {
  items:any[]
 
-  constructor(private fb:FormBuilder,private ss:ScheduleService) { }
-  scheduleForm=this.fb.group({
-    fullName:[''],
-    mobileNumber:[''],
-    email:[''],
-    address:[''],
-    area:[''],
-    pincode:[''],
-    pickupDay:[''],
-    pickupTimeSlot:[''],
-    packageId:['']
-  })
+  constructor(private fb:FormBuilder,private ss:ScheduleService) 
+  { 
+    
+  }
+  
+  
   onSubmit(){
   }
  
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+
   }
 
   
   }
+}
 
  
 
