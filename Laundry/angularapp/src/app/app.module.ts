e39e615fcbf8b658389f48d798b949144380bdf0
Iamneo <techadmin@iamneo.ai>
2023-11-25 16:51:11 +0000

    Backup Commit

---
+++ Laundry/angularapp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ScheduleDetailsComponent } from './schedule-details/schedule-details.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+    ScheduleDetailsComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Laundry/angularapp/src/app/schedule-details/schedule-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ScheduleService } from '../services/schedule-service.service';
+
+@Component({
+  selector: 'app-schedule-details',
+  templateUrl: './schedule-details.component.html',
+  styleUrls: ['./schedule-details.component.css']
+})
+export class ScheduleDetailsComponent implements OnInit {
+  schedules:any[]
+  packageOptions:any[]
+  statusOptions:any[]
+  items:any[]
+  scheduleId:number
+  statusId:number
+
+  constructor(private s:ScheduleService) { }
+
+  ngOnInit(): void {
+    this.s.getSchedules()
+    .subscribe(
+      data=>{this.schedules=data}
+    )
+    this.s.getPackages()
+    .subscribe(
+      data=>{this.packageOptions=data}
+    )
+    this.s.getStatuses()
+    .subscribe(
+      data=>{this.statusOptions=data}
+    )
+  }
+
+  
+}
