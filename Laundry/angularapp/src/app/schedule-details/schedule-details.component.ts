42593625a0c0f7f7d228e81f9818fd9d07e868e9
Iamneo <techadmin@iamneo.ai>
2023-11-25 19:36:09 +0000

    Backup Commit

--- Laundry/angularapp/src/app/schedule-details/schedule-details.component.ts
+++ Laundry/angularapp/src/app/schedule-details/schedule-details.component.ts
@@ -8,12 +8,18 @@ import { ScheduleService } from '../services/schedule-service.service';
 })
 export class ScheduleDetailsComponent implements OnInit {
   
-  
+  schedules:any[];
+  packageOptions:any[];
+  statusOptions:any[];
 
   constructor(private s:ScheduleService) { }
-  
+
 
   ngOnInit(): void {
+
+    this.s.getSchedules().subscribe(r => this.schedules=r)
+    this.s.get
+
     
    
   }
