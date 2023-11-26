84fe5ec454e14e719e62ad95a4f4c1ab3a072857
Iamneo <techadmin@iamneo.ai>
2023-11-26 07:07:35 +0000

    Backup Commit

--- BasketBall_Players/angularapp/src/app/services/players.service.ts
+++ BasketBall_Players/angularapp/src/app/services/players.service.ts
@@ -1,100 +1,44 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, scheduled } from 'rxjs';
-
+import { Observable } from 'rxjs';
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayersService {
-
-  public apiURL = "https://8080-cabbaacddbbb307795438daadeafcadfone.premiumproject.examly.io/api"
+ 
+  public apiBaseurl = "https://8080-cabbaacddbbb307795438daadeafcadfone.premiumproject.examly.io/api"
 
   constructor(private httpclient: HttpClient) { }
  
   httpOptions = { headers: new HttpHeaders({ 'Content-type': 'application/json' }) }
-   getPlayer:any;
-
-//    getPlayers():Observable<any[]>
-// {
-//   return this.httpclient.get<any[]>(this.apiURL+"/players")
-// }
-
-  getPlayers(): Observable<any[]>
+  getPlayer:any;
+ 
+  getPlayers(): Observable<any>
   {
-    return this.httpclient.get<any>(`${PlayersService['apiURL']} + "players"`)
+    return this.httpclient.get<any>(`${PlayersService['apiBaseUrl']}/players`)
   }
-
-  // getStatuses(): Observable<any[]>
+  // getStatuses():Observable<any>
   // {
   //   return this.httpclient.get<any>(`${ScheduleService['apiBaseUrl']}/laundry/status`)
-
   // }
-
-  // getSchedules(): Observable<any[]>
+  // getSchedules():Observable<any>
   // {
   //   return this.httpclient.get<any>(`${ScheduleService['apiBaseUrl']}/laundry/schedules`)
-
   // }
-
-  // createSchedule(scheduleData:any): Observable<any[]>
+ 
+  // createSchedule(scheduledata:any):Observable<any>
   // {
-  //   return this.httpclient.post<any>(`${ScheduleService['apiBaseUrl']}/laundry/schedule/add`, scheduleData, this.httpOptions)
-
+  //   return this.httpclient.post<any>(`${ScheduleService['apiBaseUrl']}/laundry/schedule/add`,scheduledata, this.httpOptions)
   // }
-
-  // updateSchedule(scheduleId:number, statusId:number): Observable<any[]>
+ 
+  // updateSchedule(scheduleId:number,statusId:number):Observable<UserSchedule>
   // {
-  //   return this.httpclient.put<any>(`${ScheduleService['apiBaseUrl']}/laundry/schedule/update/${scheduleId}/${statusId}`, scheduleId+statusId, this.httpOptions)
-
-  // }
-  
-  // deleteSchedule(scheduleId:number)
+  //   return this.httpclient.put<UserSchedule>(`${ScheduleService['apiBaseUrl']}/laundry/schedule/update/${scheduleId}/${statusId}`,this.httpOptions)
+  // }    
+ 
+  // deleteSchedule(scheduleId:number):Observable<any>
   // {
-  //   return this.httpclient.delete<any>(`${ScheduleService['apiBaseUrl']}/laundry/schedule/delete/${scheduleId}`,this.httpOptions)
-
+  //   return this.httpclient.delete<UserSchedule>(`${ScheduleService['apiBaseUrl']}/laundry/schedule/delete/${scheduleId}`)
   // }
-
 }
-
-
-
-
-
-
-
-
-// import { HttpClient } from '@angular/common/http';
-// import { Injectable } from '@angular/core';
-// import { Player } from '../models/player';
-// import { Observable } from 'rxjs';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class PlayersService {
-// public apiURL="https://8080-ffbabdadc307818048dccbfcadeone.premiumproject.examly.io/api"
-//   constructor(private httpclient:HttpClient) { }
-// getPlayers():Observable<any[]>
-// {
-//   return this.httpclient.get<any[]>(this.apiURL+"/players")
-// }
-// getPlayer(id:number):Observable<any>
-// {
-//   return this.httpclient.get<any>(this.apiURL)
-// }
-// createPlayer(player:Player):Observable<Player>
-// {
-//   return this.httpclient.post<Player>(this.apiURL,player)
-// }
-
-// updatePlayer(id:number,player:Player):Observable<void>
-// {
-//   return this.httpclient.put<void>(this.apiURL,player);
-// }
-
-// deletePlayer(id:number):Observable<void>
-// {
-//   return this.httpclient.delete<void>(this.apiURL)
-// }
-
-// }
--- BasketBall_Players/angularapp/src/app/services/positions.service.ts
+++ BasketBall_Players/angularapp/src/app/services/positions.service.ts
@@ -1,17 +1,43 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class PositionsService {
+ 
+  public apiBaseurl = "https://8080-cabbaacddbbb307795438daadeafcadfone.premiumproject.examly.io/api"
 
-  public apiURL="https://8080-ffbabdadc307818048dccbfcadeone.premiumproject.examly.io/api"
-  constructor(private httpclient:HttpClient) { }
-  
-  getPositions():Observable<any[]>
+  constructor(private httpclient: HttpClient) { }
+ 
+  httpOptions = { headers: new HttpHeaders({ 'Content-type': 'application/json' }) }
+ 
+  getPositions(): Observable<any>
   {
-    return this.httpclient.get<any[]>(this.apiURL+"/positions");
+    return this.httpclient.get<any>(`${PositionsService['apiBaseUrl']}/positions`)
   }
+  // getStatuses():Observable<any>
+  // {
+  //   return this.httpclient.get<any>(`${ScheduleService['apiBaseUrl']}/laundry/status`)
+  // }
+  // getSchedules():Observable<any>
+  // {
+  //   return this.httpclient.get<any>(`${ScheduleService['apiBaseUrl']}/laundry/schedules`)
+  // }
+ 
+  // createSchedule(scheduledata:any):Observable<any>
+  // {
+  //   return this.httpclient.post<any>(`${ScheduleService['apiBaseUrl']}/laundry/schedule/add`,scheduledata, this.httpOptions)
+  // }
+ 
+  // updateSchedule(scheduleId:number,statusId:number):Observable<UserSchedule>
+  // {
+  //   return this.httpclient.put<UserSchedule>(`${ScheduleService['apiBaseUrl']}/laundry/schedule/update/${scheduleId}/${statusId}`,this.httpOptions)
+  // }    
+ 
+  // deleteSchedule(scheduleId:number):Observable<any>
+  // {
+  //   return this.httpclient.delete<UserSchedule>(`${ScheduleService['apiBaseUrl']}/laundry/schedule/delete/${scheduleId}`)
+  // }
 }
