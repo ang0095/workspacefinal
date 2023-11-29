1c25bc343af67f0e0ed252fa765816a5c540c893
Iamneo <techadmin@iamneo.ai>
2023-11-29 13:20:36 +0000

    Backup Commit

--- Laundry/angularapp/src/app/capstoneservice/login.service.ts
+++ Laundry/angularapp/src/app/capstoneservice/login.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,13 @@ import { Injectable } from '@angular/core';
 })
 export class LoginService {
 
-  constructor(private http:) { }
+  constructor(private http:HttpClient) 
+  { 
+
+  }
+
+  getCourses()
+  {
+    
+  }
 }
