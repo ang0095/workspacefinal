8dd5c02d86ac002b67e3d3fa6b70dd46b09094d9
Iamneo <techadmin@iamneo.ai>
2023-11-29 08:49:55 +0000

    Backup Commit

--- Laundry/angularapp/src/app/app-routing.module.ts
+++ Laundry/angularapp/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ScheduleFormComponent } from './schedule-form/schedule-form.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path : '/laundry/schedule/add', component : ScheduleFormComponent}
+  {path : '/laundry/schedule/add', component : ScheduleFormComponent},
+  {path :'login' , component: LoginComponent}
   // {path : '/laundry/schedule/', component : ScheduleFormComponent}
 
 ];
