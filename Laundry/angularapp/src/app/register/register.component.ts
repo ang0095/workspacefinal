c7b416387280d05c54271d27b3c5738a0d62291b
Iamneo <techadmin@iamneo.ai>
2023-11-30 06:24:38 +0000

    Backup Commit

--- Laundry/angularapp/src/app/register/register.component.ts
+++ Laundry/angularapp/src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { EmailValidator, FormBuilder, ReactiveFormsModule, Validators,RequiredValidator } from '@angular/forms';
 import {FormArray, FormControl, FormGroup, Validator} from '@angular/forms';
 import { LoginService } from '../capstoneservice/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -13,15 +14,25 @@ export class RegisterComponent implements OnInit {
   registerForm:any;
   registers:any;
 
-  constructor(private fb:FormBuilder, private http:LoginService) 
+  constructor(private fb:FormBuilder, private http:LoginService, private routes:Router) 
   { 
     this.registerForm = this.fb.group({
       userName:['',Validators.required],
       email:['',Validators.required],
       password:['',Validators.required],
-      email:['',Validators.required],
+      mobileNumber:['',Validators.required],
+      role:['',Validators.required]
 
     })
+
+    onSubmit()
+  {
+    // this.http.getCourses().subscribe(r => this.registers=r);
+    console.log("success");
+    this.routes.navigate(['/login']);
+    
+
+  }
   }
 
 
