806b7fb4cee2984361684b3805a408f931bd14ab
Iamneo <techadmin@iamneo.ai>
2023-12-01 10:32:22 +0000

    Backup Commit

--- Laundry/angularapp/src/app/guards/oth.guard.ts
+++ Laundry/angularapp/src/app/guards/oth.guard.ts
@@ -1,15 +1,78 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, Router} from '@angular/router';
 import { Observable } from 'rxjs';
+import { LoginService } from '../capstoneservice/login.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class OthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+ 
+  constructor(private auth:LoginService, private router:Router)
+    {
+ 
+    }
+ 
+    role = localStorage.getItem("keyRole");
+ 
+  canActivate():boolean{
+   
+    if(this.auth.isLoggedIn() && this.role == "Student")
+    {
+      return true;
+    }
+   
+    else
+    {
+      alert("Error occured! Please try again.");
+      this.router.navigate(['login']);
+      return false;
+    }
+   
   }
-  
+ 
 }
+
+
+/*
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class OthGuard implements CanActivate {
+ 
+  constructor(private auth:AuthService, private router:Router)
+    {
+ 
+    }
+ 
+    role = localStorage.getItem("keyRole");
+ 
+  canActivate():boolean{
+   
+    if(this.auth.isLoggedIn() && this.role == "Organizer")
+    {
+      return true;
+    }
+   
+    else
+    {
+      alert("Error occured! Please try again.");
+      this.router.navigate(['login']);
+      return false;
+    }
+   
+  }
+ 
+}
+
+
+
+
+
+*/
