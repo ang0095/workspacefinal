faabbccae276da99f31d721fbeb7034719929147
Iamneo <techadmin@iamneo.ai>
2023-12-02 13:09:52 +0000

    Backup Commit

--- Laundry/angularapp/src/app/reset/reset.component.ts
+++ Laundry/angularapp/src/app/reset/reset.component.ts
@@ -3,6 +3,8 @@ import { ResetPassword } from '../capmodels/reset-password.model';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ConfirmPasswordValidator } from '../helpers/confirm-password.validator';
 import { LoginService } from '../capstoneservice/login.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-reset',
@@ -13,7 +15,7 @@ export class ResetComponent implements OnInit {
   resetPasswordForm!:FormGroup;
   
 
-  constructor(private fb:FormBuilder, private http:LoginService, ) 
+  constructor(private fb:FormBuilder, private http:LoginService, private route:Router) 
   { 
     this.resetPasswordForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -35,22 +37,20 @@ export class ResetComponent implements OnInit {
 
     resetPassword() {
       if (this.resetPasswordForm.valid) {
-        const  email = this.resetPasswordForm.value.email;
-        const  newPassword  = this.resetPasswordForm.value.newPassword;
+        const  {email, newPassword} = this.resetPasswordForm.value;
+        //const  newPassword  = this.resetPasswordForm.value.newPassword;
         
         console.log(email, newPassword);
 
         this.http.resetPassword(email, newPassword)
-          .subscribe({
-            next:(res =>{
-              alert(res.message)
-              this.registerForm.reset();
-              this.routes.navigate(['login']);
-            }),
-            error:(err =>{
-              alert(err?.error.message)
-            })
-      });
+          .subscribe(
+            ()=>{
+              console.log("success");
+            },
+            error=>{
+              console.error("failure");
+            }
+          );
       }
     }
   }
